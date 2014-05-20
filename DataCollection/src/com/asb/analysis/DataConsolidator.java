ff6ae4d96453ceb4739a839c3e670b1851032932
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-05-20 17:50:32 +0530

    Additional Reports Added

--- DataCollection/src/com/asb/analysis/DataConsolidator.java
+++ DataCollection/src/com/asb/analysis/DataConsolidator.java
@@ -6,37 +6,44 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class DataConsolidator {
 
-  public static final String CONSOLIDATED_REPORT = "001_Consolidated_Report.csv";
+  public static final String CONSOLIDATED_REPORT = "../0_Consolidated_Report.csv";
   public int recordNo = 0;
-  
+
   public void migrateData() {
     File path = new File(".");
     File consolidatedReport = new File(CONSOLIDATED_REPORT);
     BufferedReader constituencyFile = null;
     BufferedWriter bwReportFile = null;
+    Set<String> consList = new HashSet<String>();
     recordNo = 0;
+    int fileCounter = 0;
     try {
       bwReportFile = new BufferedWriter(new FileWriter(consolidatedReport));
-      bwReportFile.write("ID,STATE,CONSTITUENCY,CANDIDATE,PARTY,VOTES\n");
+      // bwReportFile.write("ID,STATE,CONSTITUENCY,CANDIDATE,PARTY,VOTES\n");
       File[] files = path.listFiles();
       for (File file : files) {
         if (file.isFile()) {
           String fileName = file.getName();
-          if (fileName.endsWith(".csv") && !fileName.startsWith(CONSOLIDATED_REPORT)) {
-            System.out.println("Processing: " + fileName);
-            fileName = fileName.substring(fileName.indexOf(" "),
-                fileName.indexOf("."));
+          if (fileName.endsWith(".csv")) {
+            int endIndex = fileName.length() - 4;
+            fileName = fileName.substring(fileName.indexOf(" "), endIndex);
             String[] splits = fileName.split(" - ");
             String state = splits[0].trim();
             String constituency = splits[1].trim();
-            try {              
+            fileCounter += 1;
+            System.out.println(fileCounter + ". Processing: " + fileName);
+            consList.add(constituency.toUpperCase(Locale.US));
+            try {
               constituencyFile = new BufferedReader(new FileReader(file));
-              consolidateData(bwReportFile, constituencyFile, state, constituency);
+              consolidateData(bwReportFile, constituencyFile, state,
+                  constituency);
             } catch (IOException e) {
               e.printStackTrace();
             } finally {
@@ -65,26 +72,28 @@ public class DataConsolidator {
   }
 
   private void consolidateData(BufferedWriter bwReportFile,
-      BufferedReader constituencyFile, String state, String constituency) throws IOException {
-      String row = constituencyFile.readLine();
-      while (null != (row = constituencyFile.readLine())) {
-        StringTokenizer st = new StringTokenizer(row, ",");
-        String candidate = st.nextToken().trim();
-        String party = st.nextToken().trim();
-        String votes = st.nextToken().trim();
-        recordNo += 1;
-        bwReportFile.write(String.valueOf(recordNo));
-        bwReportFile.write(",");
-        bwReportFile.write(state.toUpperCase(Locale.US));
-        bwReportFile.write(",");
-        bwReportFile.write(constituency.toUpperCase(Locale.US));
-        bwReportFile.write(",");
-        bwReportFile.write(candidate.toUpperCase(Locale.US));
-        bwReportFile.write(",");
-        bwReportFile.write(party.toUpperCase(Locale.US));
-        bwReportFile.write(",");
-        bwReportFile.write(votes);
-        bwReportFile.write("\n");
-      }
+      BufferedReader constituencyFile, String state, String constituency)
+      throws IOException {
+    String row = constituencyFile.readLine();
+    while (null != (row = constituencyFile.readLine())) {
+      StringTokenizer st = new StringTokenizer(row, ",");
+      String candidate = st.nextToken().trim();
+      String party = st.nextToken().trim();
+      String votes = st.nextToken().trim();
+      recordNo += 1;
+      bwReportFile.write(String.valueOf(recordNo));
+      bwReportFile.write(",");
+      bwReportFile.write(state.replaceAll("\"", "").toUpperCase(Locale.US));
+      bwReportFile.write(",");
+      bwReportFile.write(constituency.replaceAll("\"", "").toUpperCase(
+          Locale.US));
+      bwReportFile.write(",");
+      bwReportFile.write(candidate.replaceAll("\"", "").toUpperCase(Locale.US));
+      bwReportFile.write(",");
+      bwReportFile.write(party.replaceAll("\"", "").toUpperCase(Locale.US));
+      bwReportFile.write(",");
+      bwReportFile.write(votes);
+      bwReportFile.write("\n");
+    }
   }
 }
